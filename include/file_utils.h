7b41aa4d229404e286bdb146274f9d8e0156a88d
Aspolone <aspogame@gmail.com>
2025-06-13 17:37:42 +0200

    Bug fix testing

--- include/file_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef VETTORE_TEST_H
-#define VETTORE_TEST_H
-
-void finput_array(char *file_name, int a[], int n);
-void foutput_array(char *file_name, int a[], int n);
-confronta_array(int a[], int b[], int n);
-
-#endif //VETTORE_TEST_H
